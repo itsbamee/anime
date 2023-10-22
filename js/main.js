bc6cd55c207d86c8a48ebadf15873f8c651d5847
chojeongeun <juy0816@naver.com>
2023-10-22 11:51:15 +0900

    scroll 모션 콜백처리

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector('button');
-const box = document.querySelector('article');
+// const box = document.querySelector('article');
 
 /*
   new Anime(선택자, {변경할 속성객체}, {duration:시간, easeType: 가속도타입, callback: 완료함수})
@@ -9,22 +9,13 @@ const box = document.querySelector('article');
 
 btn.addEventListener('click', () => {
 	new Anime(
-		box,
-		{ left: '80%', height: 200 },
+		window,
+		{ scroll: 3000 },
 		{
-			duration: 500,
+			duration: 1000,
 			easeType: 'linear',
 			callback: () => {
-				new Anime(
-					box,
-					{ backgroundColor: '#ff69b4' },
-					{
-						duration: 2000,
-						callback: () => {
-							new Anime(box, { opacity: 0 });
-						},
-					}
-				);
+				new Anime(window, { scroll: 1000 }, { duration: 600, easeType: 'ease1' });
 			},
 		}
 	);
